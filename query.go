d2a6e76b2a0ff6823d9137ab7b269d44b11363e2
juragan360 <ariefda@hotmail.com>
2015-11-11 18:30:55 +0700

    handling query part

--- query.go
+++ query.go
@@ -1,5 +1,24 @@
 package dbox
 
+type QueryPartType string
+
+const (
+	QueryPart_Select  = "SELECT"
+	QueryPart_From    = "FROM"
+	QueryPart_Where   = "WHERE"
+	QueryPart_Group   = "GROUP BY"
+	QueryPart_Order   = "ORDER BY"
+	QueryPart_Insert  = "INSERT"
+	QueryPart_Update  = "UPDATE"
+	QueryPart_Delete  = "DELETE"
+	QueryPart_Save    = "SAVE"
+	QueryPart_Command = "COMMAND"
+
+	QueryPart_Join      = "JOIN"
+	QueryPart_LeftJoin  = "LEFT JOIN"
+	QueryPart_RightJoin = "RIGHT JOIN"
+)
+
 type IQuery interface {
 	Cursor() (*Cursor, error)
 
@@ -10,8 +29,31 @@ type IQuery interface {
 	Where(...*Filter) IQuery
 }
 
+type QueryPart struct {
+	PartType string
+	Value    interface{}
+}
+
 type Query struct {
 	thisQuery IQuery
+
+	Parts []*QueryPart
+}
+
+func (q *Query) this() IQuery {
+	if q.thisQuery == nil {
+		return q
+	} else {
+		return q.thisQuery
+	}
+}
+
+func (q *Query) addPart(qp *QueryPart) IQuery {
+	if q.Parts == nil {
+		q.Parts = []*QueryPart{}
+	}
+	q.Parts = append(q.Parts, qp)
+	return q.this()
 }
 
 func (q *Query) SetThis(t IQuery) IQuery {
