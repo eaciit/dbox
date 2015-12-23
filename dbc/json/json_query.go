8cf9a1328f748950aa43a3ffaf4603b1d5d2928f
ranggaeaciit <rangga.hendradita@eaciit.com>
2015-12-23 09:47:47 +0700

    error handling
    update error handling on query function

--- dbc/json/json_query.go
+++ dbc/json/json_query.go
@@ -227,7 +227,10 @@ func (q *Query) Exec(parm toolkit.M) error {
 			return errorlib.Error(packageName, modQuery+".Exec", commandType, e.Error())
 		}
 
-		q.Connection().(*Connection).CloseWriteSession()
+		err = q.Connection().(*Connection).CloseWriteSession()
+		if err != nil {
+			return errorlib.Error(packageName, modQuery+".Exec", commandType, err.Error())
+		}
 	} else if commandType == dbox.QueryPartUpdate {
 		if multi {
 			// 		_, e = mgoColl.UpdateAll(where, data)
@@ -248,13 +251,20 @@ func (q *Query) Exec(parm toolkit.M) error {
 				return errorlib.Error(packageName, modQuery+".Exec", commandType, e.Error())
 			}
 
-			q.Connection().(*Connection).WriteSession()
+			err = q.Connection().(*Connection).WriteSession()
+			if err != nil {
+				return errorlib.Error(packageName, modQuery+".Exec", commandType, err.Error())
+			}
+
 			i, e := q.Connection().(*Connection).openFile.Write(jsonUpdatedValue) //t.WriteString(string(j))
 			if i == 0 || e != nil {
 				return errorlib.Error(packageName, modQuery+".Exec", commandType, e.Error())
 			}
 
-			q.Connection().(*Connection).CloseWriteSession()
+			err = q.Connection().(*Connection).CloseWriteSession()
+			if err != nil {
+				return errorlib.Error(packageName, modQuery+".Exec", commandType, err.Error())
+			}
 		}
 	} else if commandType == dbox.QueryPartDelete {
 		if multi {
@@ -275,13 +285,20 @@ func (q *Query) Exec(parm toolkit.M) error {
 					return errorlib.Error(packageName, modQuery+".Exec", commandType, e.Error())
 				}
 
-				q.Connection().(*Connection).WriteSession()
+				err = q.Connection().(*Connection).WriteSession()
+				if err != nil {
+					return errorlib.Error(packageName, modQuery+".Exec", commandType, err.Error())
+				}
+
 				i, e := q.Connection().(*Connection).openFile.Write(jsonUpdatedValue) //t.WriteString(string(j))
 				if i == 0 || e != nil {
 					return errorlib.Error(packageName, modQuery+".Exec", commandType, e.Error())
 				}
 
-				q.Connection().(*Connection).CloseWriteSession()
+				err = q.Connection().(*Connection).CloseWriteSession()
+				if err != nil {
+					return errorlib.Error(packageName, modQuery+".Exec", commandType, err.Error())
+				}
 			} else {
 				e := os.Remove(filePath)
 				if e != nil {
