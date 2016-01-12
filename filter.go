1b918de775c97fcdc72f94117cbf0778e1ffa5c1
satriasm <satria.sm@eaciit.com>
2016-01-12 18:14:58 +0700

    add Ne, In, Nin, Gt, Gte, Lt, Lte
    add Ne, In, Nin, Gt, Gte, Lt, Lte for cursor

--- filter.go
+++ filter.go
@@ -136,3 +136,56 @@ func Ne(field string, value interface{}) *Filter {
 	f.Value = value
 	return f
 }
+
+
+func In(field string, value []string) *Filter {
+	f := new(Filter)
+	f.Field = field
+	f.Op = string(FilterOpIn)
+	f.Value = value
+	return f
+}
+
+func Nin(field string, value []string) *Filter {
+	f := new(Filter)
+	f.Field = field
+	f.Op = string(FilterOpNin)
+	f.Value = value
+	return f
+}
+
+func Gt(field string, value int) *Filter {
+
+	f := new(Filter)
+	f.Field = field
+	f.Op = string(FilterOpGt)
+	f.Value = value
+	return f
+}
+
+func Gte(field string, value int) *Filter {
+
+	f := new(Filter)
+	f.Field = field
+	f.Op = string(FilterOpGte)
+	f.Value = value
+	return f
+}
+
+func Lt(field string, value int) *Filter {
+
+	f := new(Filter)
+	f.Field = field
+	f.Op = string(FilterOpLt)
+	f.Value = value
+	return f
+}
+
+func Lte(field string, value int) *Filter {
+
+	f := new(Filter)
+	f.Field = field
+	f.Op = string(FilterOpLte)
+	f.Value = value
+	return f
+}
