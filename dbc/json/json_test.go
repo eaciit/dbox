89e3b533a00e9ed6b245d6cb868edf0e09e2d8eb
ranggaeaciit <rangga.hendradita@eaciit.com>
2016-01-15 08:41:00 +0700

    update json fetch
    update for fetch data

--- dbc/json/json_test.go
+++ dbc/json/json_test.go
@@ -75,7 +75,7 @@ func TestSelect(t *testing.T) {
 	if e != nil {
 		t.Errorf("Unable to fetch all: %s \n", e.Error())
 	} else {
-		fmt.Printf("Fetch all OK. Result: %d \n", len(results))
+		fmt.Printf("Fetch all OK. Result: %v \n", len(results))
 	}
 
 	e = csr.ResetFetch()
@@ -84,13 +84,21 @@ func TestSelect(t *testing.T) {
 	}
 
 	//ds, e = csr.Fetch(nil, 3, false)
-	resultToN := make([]toolkit.M, 0)
-	e = csr.Fetch(&resultToN, 3, false)
+	// resultToN := make([]toolkit.M, 0)
+	e = csr.Fetch(&results, 3, false)
 	if e != nil {
 		t.Errorf("Unable to fetch N: %s \n", e.Error())
 	} else {
-		fmt.Printf("Fetch N OK. Result: %v \n",
-			resultToN)
+		fmt.Printf("Fetch N3 OK. Result: %v \n",
+			results)
+	}
+
+	e = csr.Fetch(&results, 4, false)
+	if e != nil {
+		t.Errorf("Unable to fetch N: %s \n", e.Error())
+	} else {
+		fmt.Printf("Fetch N4 OK. Result: %v \n",
+			results)
 	}
 }
 
@@ -192,7 +200,7 @@ func TestCRUD(t *testing.T) {
 	}
 
 	q := c.NewQuery().SetConfig("multiexec", true).Save()
-	for i := 1; i <= 10; i++ {
+	for i := 1; i <= 10000; i++ {
 		//go func(q dbox.IQuery, i int) {
 		data := user{}
 		data.Id = fmt.Sprintf("User-%d", i)
