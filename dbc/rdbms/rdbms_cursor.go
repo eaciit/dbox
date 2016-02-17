495784282423a05091d14a9f4038434fc409fe54
Rino Sukmandityo <rino.sukmandityo@gmail.com>
2016-02-17 14:19:11 +0700

    solved conflict, new HiveConfig and fetch update

--- dbc/rdbms/rdbms_cursor.go
+++ dbc/rdbms/rdbms_cursor.go
@@ -3,17 +3,7 @@ package rdbms
 import (
 	"database/sql"
 	"errors"
-<<<<<<< HEAD
 	"github.com/eaciit/dbox"
-=======
-<<<<<<< HEAD
-	"fmt"
-	"github.com/rinosukmandityo/dbox"
-=======
-	//"fmt"
-	"github.com/ranggadablues/dbox"
->>>>>>> bbe204ed9e388ba424883a5ce94877c03ef0bba5
->>>>>>> refs/remotes/origin/master
 	//"github.com/eaciit/errorlib"
 	"github.com/eaciit/cast"
 	"github.com/eaciit/hdc/hive"
@@ -44,7 +34,6 @@ type Cursor struct {
 	sessionHive *hive.Hive
 	session     sql.DB
 	QueryString string
-	datas       []toolkit.M
 }
 
 func (c *Cursor) Close() {
@@ -67,128 +56,91 @@ func (c *Cursor) ResetFetch() error {
 
 func (c *Cursor) Fetch(m interface{}, n int, closeWhenDone bool) error {
 	tableData := []toolkit.M{}
+	var e error
 	h := c.sessionHive
 	if h != nil {
-<<<<<<< HEAD
-		// var DoSomething = func(res string) {
-		// 	tmp := Sample7{}
-		// 	h.ParseOutput(res, &tmp)
-		// 	fmt.Println(tmp)
-		// }
-
-		// e := h.ExecLine(c.QueryString, DoSomething)
-		// fmt.Printf("error: \n%v\n", e)
-
-		_, e := h.Exec(c.QueryString)
-
-		if e != nil {
-			fmt.Printf("error: \n%v\n", e)
-		}
-
-		h.ParseOutput("", m)
-=======
-		var e error
 		var DoSomething = func(res string) {
 			fields := toolkit.M{}
 			h.ParseOutput(res, &fields)
-			c.datas = append(c.datas, fields)
+			tableData = append(tableData, fields)
 		}
 
 		e = h.ExecLine(c.QueryString, DoSomething)
 		if e != nil {
 			return e
 		}
+	} else {
 
-		first := 0
-		last := 0
-		c.count = toolkit.SliceLen(c.datas)
-		if n == 0 {
-			last = c.count
+		rows, e := c.session.Query(c.QueryString)
+		var valueType reflect.Type
+
+		if n == 1 {
+			valueType = reflect.TypeOf(m).Elem()
 		} else {
-			last = n
+			valueType = reflect.TypeOf(m).Elem().Elem()
 		}
->>>>>>> bbe204ed9e388ba424883a5ce94877c03ef0bba5
 
-		tableData = c.datas[first:last]
-
-		e = toolkit.Serde(tableData, m, "json")
 		if e != nil {
 			return e
 		}
-		return nil
-	}
-
-	rows, e := c.session.Query(c.QueryString)
-	var valueType reflect.Type
-
-	if n == 1 {
-		valueType = reflect.TypeOf(m).Elem()
-	} else {
-		valueType = reflect.TypeOf(m).Elem().Elem()
-	}
-
-	if e != nil {
-		return e
-	}
-	defer rows.Close()
-	columns, e := rows.Columns()
-	if e != nil {
-		return e
-	}
-
-	count := len(columns)
-
-	//tableData := []toolkit.M{}
-	values := make([]interface{}, count)
-	valuePtrs := make([]interface{}, count)
-
-	for rows.Next() {
-		for i := 0; i < count; i++ {
-			valuePtrs[i] = &values[i]
+		defer rows.Close()
+		columns, e := rows.Columns()
+		if e != nil {
+			return e
 		}
 
-		rows.Scan(valuePtrs...)
-		entry := toolkit.M{}
+		count := len(columns)
 
-		for i, col := range columns {
-			var v interface{}
-			val := values[i]
-			b, ok := val.([]byte)
-			if ok {
-				v = string(b)
-			} else {
-				v = val
+		//tableData := []toolkit.M{}
+		values := make([]interface{}, count)
+		valuePtrs := make([]interface{}, count)
+
+		for rows.Next() {
+			for i := 0; i < count; i++ {
+				valuePtrs[i] = &values[i]
 			}
-			entry.Set(strings.ToLower(col), v)
-		}
 
-		if valueType.Kind() == reflect.Struct {
-			for i := 0; i < valueType.NumField(); i++ {
-				namaField := strings.ToLower(valueType.Field(i).Name)
-				dataType := strings.ToLower(valueType.Field(i).Type.String())
+			rows.Scan(valuePtrs...)
+			entry := toolkit.M{}
 
-				if entry.Has(namaField) {
-					if strings.Contains(dataType, "int") {
-						entry.Set(namaField,
-							cast.ToInt(entry[namaField], cast.RoundingAuto))
-					} else if strings.Contains(dataType, "time.time") {
-						entry.Set(namaField,
-							cast.String2Date(cast.ToString(entry[namaField]), "2006-01-02 15:04:05"))
+			for i, col := range columns {
+				var v interface{}
+				val := values[i]
+				b, ok := val.([]byte)
+				if ok {
+					v = string(b)
+				} else {
+					v = val
+				}
+				entry.Set(strings.ToLower(col), v)
+			}
+
+			if valueType.Kind() == reflect.Struct {
+				for i := 0; i < valueType.NumField(); i++ {
+					namaField := strings.ToLower(valueType.Field(i).Name)
+					dataType := strings.ToLower(valueType.Field(i).Type.String())
+
+					if entry.Has(namaField) {
+						if strings.Contains(dataType, "int") {
+							entry.Set(namaField,
+								cast.ToInt(entry[namaField], cast.RoundingAuto))
+						} else if strings.Contains(dataType, "time.time") {
+							entry.Set(namaField,
+								cast.String2Date(cast.ToString(entry[namaField]), "2006-01-02 15:04:05"))
+						}
 					}
 				}
 			}
-		}
 
-		tableData = append(tableData, entry)
+			tableData = append(tableData, entry)
+		}
 	}
 
-	// fmt.Println("Nilai table data : ", tableData)
 	if e != nil {
 		return e
 	}
 	if n == 0 {
 		e = toolkit.Serde(tableData, m, "json")
-		// fmt.Println("Nilai Model : ", m)
 	} else {
 		end := c.start + n
 		if end > len(tableData) {
